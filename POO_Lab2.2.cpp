f7532e5cdb7e9e4bbb81f5bc96454792f2240b0e
Dan <danmgame0n@gmail.com>
2023-11-11 20:29:14 +0200

    Corectarea programului!
    S-a refăcut programul de la 0 pentru a înțelege de ce nu a funcționat corespunzător. S-a supraîncărcat operatorul = și s-a adăugat un constructor de copiere.

--- POO_Lab2.2.cpp
+++ POO_Lab2.2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <time.h>
 #include <iomanip>
 using namespace std;
@@ -9,25 +10,24 @@ class Complex
 	float im; //partea imaginara a numarului
 	public:
 		//constructor implicit
-		Complex() : re(0), im(0) {}
+		Complex() : re(0.0), im(0.0) {}
 
-		//constructor cu doi parametri pentru initializarea re si im
+		//constructor parametrizat pentru initializarea re si im
 		Complex(float real, float imag) : re(real), im(imag) {}
-
 		float getReal() { return re; } //functie de returnare a partii reale
 		float getImag() { return im; } //functie de returnare a partii imaginare
-
-		Complex operator+(Complex& otherMx) //supraincarcarea operatorului + 
+		
+		Complex operator+(const Complex& otherMx) //supraincarcarea operatorului +
 		{
-			return Complex(re + otherMx.re, im + otherMx.im); //returneaza un obiect cu noul rezultat
+			return Complex(re + otherMx.re, im + otherMx.im);
 		}
 
-		Complex operator-(Complex& otherMx) //supraincarcarea operatorului -
+		Complex operator-(const Complex& otherMx) //supraincarcarea operatorului -
 		{
 			return Complex(re - otherMx.re, im - otherMx.im);
 		}
 
-		Complex operator*(Complex& otherMx) //supraincarcarea operatorului *
+		Complex operator*(const Complex& otherMx) //supraincarcarea operatorului *
 		{
 			return Complex(re * otherMx.re, im * otherMx.im);
 		}
@@ -55,39 +55,60 @@ class Matrix
 			alocareMemorie(r_size, c_size);
 		}
 
-		//desctrutor
+		//constructor de copiere
+		Matrix(const Matrix& otherMx) : randuri(otherMx.randuri), coloane(otherMx.coloane), codEroare(otherMx.codEroare)
+		{
+			alocareMemorie(randuri, coloane);
+
+			for (int i = 0; i < randuri; i++)
+				for (int j = 0; j < coloane; j++)
+				{
+					C[i][j] = otherMx.C[i][j];
+				}
+		}
+
+		//destructor
 		~Matrix()
 		{
+			if (C != nullptr)
+			{
+				for (int i = 0; i < randuri; i++)
+				{
+					delete[] C[i];
+				}
+				delete[] C;
+				C = nullptr;
+			}
 			//cout << "Destructor: Obiect distrus!\n";
 		}
 
-		Matrix alocareMemorie(int r_size, int c_size) //functie de alocare a memoriei pentru matrice
+		//functie de alocare de memorie pentru ambele matrici
+		void alocareMemorie(int r_size, int c_size)
 		{
-			if (r_size > 0 && c_size > 0)
-			{
-				C = new Complex * [r_size]; //aloca memorie pentru un array de pointeri
-				for (int i = 0; i < r_size; i++)
-					C[i] = new Complex[c_size]; //aloca memorie de c_size coloane pentru fiecare linie
-			}
-			else
+			if (r_size < 0 && c_size < 0)
 			{
 				codEroare = 1; //cod de eroare pentru dimensiuni invalide
-				return Matrix(); //returneaza un obiect pentru a indica eroarea
+				return;
 			}
+			C = new Complex * [r_size]; //aloca memorie pentru un array de pointeri
+			for (int i = 0; i < r_size; i++)
+				C[i] = new Complex[c_size]; //aloca memorie de c_size coloane pentru fiecare linie
+			//cout << "Memorie alocata!\n";
 		}
 
-		//functie de creare a matricelor patrate sau dreptunghiulare
+		//functie de creare a ambelor matrici
 		void creareMatrice(int r_size, int c_size)
 		{
-			int optiune;
+			int optiune = 0;
 			float re, im;
-			cout << "\nAlege prin ce metoda sa introduci elementele: "
-				<< "\n1. De la tastatura"
-				<< "\n2. Random"
-				<< "\nOptiunea aleasa: ";
-			cin >> optiune;
 			do
 			{
+				cout << "\nAlege prin ce metoda sa introduci elementele: "
+					<< "\n1. De la tastatura"
+					<< "\n2. Random"
+					<< "\n3. Iesire"
+					<< "\nOptiunea aleasa: ";
+				cin >> optiune;
 				switch (optiune)
 				{
 				case 1:
@@ -115,13 +136,15 @@ class Matrix
 						}
 					cout << "\nElementele au fost generate aleatoriu.\n";
 					break;
+				case 3: cout << "\nIesire din functie!\n";
+					break;
 				default: cout << "\nOptiune invalida! Incearca din nou.\n";
 					break;
 				}
-			} while (optiune != 1 && optiune != 2);
+			} while (optiune != 1 && optiune != 2 && optiune != 3);
 		}
 
-		//afisarea matricelor la ecran
+		//functie de afisare a matricelor la ecran
 		void afisareMatrice()
 		{
 			cout << endl;
@@ -129,114 +152,156 @@ class Matrix
 			{
 				for (int j = 0; j < coloane; j++)
 				{
-					//cout << fixed << setprecision(1); 
-					cout.precision(2); //afisarea valorilor cu o singura zecimala dupa virgula
+					cout << fixed << setprecision(1); 
+					//cout.precision(2); //afisarea valorilor cu o singura zecimala dupa virgula
 					cout << C[i][j].getReal() << " + " << C[i][j].getImag() << "i   "; //afiseaza partea reala si imaginara
 				}
 				cout << endl;
 			}
 		}
-
+		
+		//functie de setare a valorilor
 		void setElem(int i, int j, const Complex& newValue)
 		{
-			if (i >= 0 && i < randuri && j >= 0 && j < coloane) //verifica daca indicii nu sunt in afara limitelor
-				C[i][j] = newValue; //seteaza noua valoarea pe randul i si coloana j
+			if (i >= 0 && i < randuri && j >= 0 && j < coloane) C[i][j] = newValue;
+			else codEroare = 2; //cod de eroare pentru coordonate in afara limitei matricei
 		}
 
-		//functie de adunare a doua matrici
-		Matrix adunare(const Matrix& otherMx)
+		Matrix operator+(const Matrix& otherMx)
 		{
 			if (randuri != otherMx.randuri || coloane != otherMx.coloane)
 			{
-				codEroare = 2; //cod de eroare pentru dimensiuni incompatibile
-				return Matrix();
+				codEroare = 3; //cod de eroare pentru dimensiuni incompatibile
+				return Matrix(); //returneaza un obiect gol
 			}
-			else codEroare = 0; //daca dimensiuni sunt asemanatoare, codEroare este setat la 0 pentru a indica ca nu sunt erori
-			Matrix suma(randuri, coloane); //se creeaza o matrice noua cu aceleasi dimensiuni
+			else codEroare = 0; //reseteaza cod de eroare
+
+			Matrix suma(randuri, coloane); //obiect de tip Matrix cu randuri x coloane
 			for (int i = 0; i < randuri; i++)
+			{
 				for (int j = 0; j < coloane; j++)
 				{
-					Complex sumaElem = C[i][j] + otherMx.C[i][j]; //se aduna elementele curente cu elementele din matricea 2
-					suma.setElem(i, j, sumaElem); //seteaza elementul pe pozitia i j
+					//suma.C[i][j] = C[i][j] + otherMx.C[i][j];
+					Complex sumaElem = C[i][j] + otherMx.C[i][j];
+					suma.setElem(i, j, sumaElem);
 				}
-			return suma; //returneaza matricea
+			}
+			return suma;
 		}
 
-		//scadere a doua matrici
-		Matrix scadere(const Matrix& otherMx)
+		Matrix operator-(const Matrix& otherMx)
 		{
 			if (randuri != otherMx.randuri || coloane != otherMx.coloane)
 			{
-				codEroare = 2; //cod de eroare pentru dimensiuni incompatibile
+				codEroare = 3;
 				return Matrix();
 			}
 			else codEroare = 0;
+
 			Matrix diferenta(randuri, coloane);
 			for (int i = 0; i < randuri; i++)
+			{
 				for (int j = 0; j < coloane; j++)
 				{
-					Complex diferentaElem = C[i][j] - otherMx.C[i][j];
-					diferenta.setElem(i, j, diferentaElem);
+					//diferenta.C[i][j] = C[i][j] - otherMx.C[i][j];
+					Complex difElem = C[i][j] - otherMx.C[i][j];
+					diferenta.setElem(i, j, difElem);
 				}
+			}
 			return diferenta;
 		}
 
-		//inmultire a doua matrici
-		Matrix inmultire(const Matrix& otherMx)
+		Matrix operator*(const Matrix& otherMx)
 		{
 			if (randuri != otherMx.randuri || coloane != otherMx.coloane)
 			{
-				codEroare = 2; //cod de eroare pentru dimensiuni incompatibile
+				codEroare = 3;
 				return Matrix();
 			}
 			else codEroare = 0;
+
 			Matrix produs(randuri, coloane);
 			for (int i = 0; i < randuri; i++)
+			{
 				for (int j = 0; j < coloane; j++)
 				{
-					Complex produsElem = C[i][j] * otherMx.C[i][j];
-					produs.setElem(i, j, produsElem);
+					//produs.C[i][j] = C[i][j] * otherMx.C[i][j];
+					Complex prodElem = C[i][j] * otherMx.C[i][j];
+					produs.setElem(i, j, prodElem);
 				}
+			}
 			return produs;
 		}
 
-		//inmultirea unei matrici cu un numar
+		//functie de inmultire a unui numar cu fiecare element dintr-o matrice
 		void inmultireCuNumar(int nr)
 		{
 			for (int i = 0; i < randuri; i++)
+			{
 				for (int j = 0; j < coloane; j++)
 				{
-					C[i][j] = Complex(C[i][j].getReal() * nr, C[i][j].getImag() * nr); //actualizeaza fiecare element din matrice inmultit la numarul dorit
+					C[i][j] = Complex(C[i][j].getReal() * nr, C[i][j].getImag() * nr);
 				}
+			}
 		}
 
-		int getCodEroare() //functie de returnare a codului de eroare
+		//supraincarcarea operatorului = 
+		Matrix& operator=(const Matrix& otherMx)
 		{
-			return codEroare;
+			if (this != &otherMx) //evita autoatribuirea
+			{
+				//elibereaza memoria existenta
+				if (C != nullptr)
+				{
+					for (int i = 0; i < randuri; i++)
+					{
+						delete[] C[i];
+					}
+					delete[] C;
+					C = nullptr;
+				}
+
+				//copiaza datele din cealalta matrice
+				randuri = otherMx.randuri;
+				coloane = otherMx.coloane;
+				codEroare = otherMx.codEroare;
+
+				//aloca memorie pentru matricea curenta
+				alocareMemorie(randuri, coloane);
+
+				//copiaza elementele din cealalta matrice
+				for (int i = 0; i < randuri; i++)
+				{
+					for (int j = 0; j < coloane; j++)
+					{
+						C[i][j] = otherMx.C[i][j];
+					}
+				}
+			}
+			return *this; //returneaza o referinta la obiectul curent
 		}
 
-		bool notEmpty() //functie ajutatoare pentru verificare
-		{
-			return (C != nullptr);
-		}
+		int getCodEroare() { return codEroare; } //functie de returnare a codului de eroare
+
+		bool notEmpty() { return (C != nullptr); } //functie de returnare true/false in dependenta de pointer
+
+		void setNullptr() { C = nullptr; } //functie de setare a unui pointer ca nullptr
 };
 
 //functie ajutatoare pentru verificare inainte de afisare pentru a evita afisarea matricelor goale/inexistente
-void verificarePtAfisare(Matrix& m, const string& numeMatrice)
+void verificarePtAfisare(Matrix& matrix, const string& numeMatrice)
 {
-	if (m.notEmpty()) //verifica daca matricea nu este goala
+	if (matrix.notEmpty())
 	{
 		cout << "\n" << numeMatrice << ":";
-		m.afisareMatrice();
+		matrix.afisareMatrice();
 	}
 }
 
-int main()
-{
-	Matrix p, d, suma, diferenta, produs; //obiecte de tip Matrix
-	int optiune;
-	do
-	{
+int main() {
+	Matrix p, d, suma, diferenta, produs;
+	int optiune = 0;
+	do {
 		cout << "\n<<----------Meniu---------->>"
 			<< "\n1. Creeaza o matrice patrata"
 			<< "\n2. Creeaza o matrice dreptunghiulara"
@@ -251,90 +316,105 @@ int main()
 		switch (optiune)
 		{
 		case 1:
-				int size;
-				cout << "\nIntrodu dimensiunea matricei patratice: ";
-				cin >> size;
-				p = Matrix(size);
-				if (p.getCodEroare() == 1) cout << "\nEroare: Dimensiune invalida!\n";
-				else p.creareMatrice(size, size);
+			int size;
+			cout << "\nIntrodu dimensiunea matricei patratice: ";
+			cin >> size;
+			p = Matrix(size);
+			if (p.getCodEroare() == 1) cout << "\nEroare: Dimensiune invalida!\n";
+			else p.creareMatrice(size, size);
 			break;
 		case 2:
-				int r_size, c_size;
-				cout << "\nIntrodu dimensiunile matricei dreptunghiulare:\nNr. Randuri: ";
-				cin >> r_size;
-				cout << "Nr. Coloane: ";
-				cin >> c_size;
-				d = Matrix(r_size, c_size);
-				if (d.getCodEroare() == 1) cout << "\nEroare: Dimensiuni invalide!\n";
-				else d.creareMatrice(r_size, c_size);
+			int r_size, c_size;
+			cout << "\nIntrodu dimensiunile matricei dreptunghiulare:\nNr. Randuri: ";
+			cin >> r_size;
+			cout << "Nr. Coloane: ";
+			cin >> c_size;
+			d = Matrix(r_size, c_size);
+			if (d.getCodEroare() == 1) cout << "\nEroare: Dimensiuni invalide!\n";
+			else d.creareMatrice(r_size, c_size);
 			break;
-		case 3: 
-				if (p.notEmpty() || d.notEmpty())
-				{
-					verificarePtAfisare(p, "Matricea patrata");
-					verificarePtAfisare(d, "Matricea dreptunghiulara");
-					verificarePtAfisare(suma, "Suma matricelor");
-					verificarePtAfisare(diferenta, "Diferenta matricelor");
-					verificarePtAfisare(produs, "Produsul matricelor");
-				}
-				else cout << "\nMatricile nu au fost create!\n";
+		case 3:
+			if (p.notEmpty() || d.notEmpty())
+			{
+				verificarePtAfisare(p, "Matricea patrata");
+				verificarePtAfisare(d, "Matricea dreptunghiulara");
+				verificarePtAfisare(suma, "Suma matricelor");
+				verificarePtAfisare(diferenta, "Diferenta matricelor");
+				verificarePtAfisare(produs, "Produsul matricelor");
+			}
+			else cout << "\nMatricile nu au fost create!\n";
 			break;
 		case 4:
-				suma = p.adunare(d);
-				if (p.getCodEroare() == 2) cout << "\nEroare: Dimensiuni incompatibile!\n";
-				else
-				{
-					cout << "\nRezultatul matricelor adunate:";
-					suma.afisareMatrice();
-				}
+			suma = p + d;
+			if (p.getCodEroare() == 3)
+			{
+				suma.setNullptr();
+				cout << "\nEroare: Dimensiuni incompatibile!\n";
+			}
+			else
+			{
+				cout << "\nRezultatul matricelor adunate:";
+				suma.afisareMatrice();
+			}
 			break;
-		case 5: diferenta = p.scadere(d);
-				if (p.getCodEroare() == 2) cout << "\nEroare: Dimensiuni incompatibile!\n";
-				else
-				{
-					cout << "\nRezultatul diferentei matricelor:";
-					diferenta.afisareMatrice();
-				}
+		case 5:
+			diferenta = p - d;
+			if (p.getCodEroare() == 3)
+			{
+				diferenta.setNullptr();
+				cout << "\nEroare: Dimensiuni incompatibile!\n";
+			}
+			else
+			{
+				cout << "\nRezultatul diferentei matricelor:";
+				diferenta.afisareMatrice();
+			}
 			break;
-		case 6: produs = p.inmultire(d);
-				if (p.getCodEroare() == 2) cout << "\nEroare: Dimensiuni incompatibile!\n";
-				else
-				{
-					cout << "\nRezultatul produsului matricelor:";
-					produs.afisareMatrice();
-				}
+		case 6:
+			produs = p * d;
+			if (p.getCodEroare() == 3)
+			{
+				produs.setNullptr();
+				cout << "\nEroare: Dimensiuni incompatibile!\n";
+			}
+			else
+			{
+				cout << "\nRezultatul produsului matricelor:";
+				produs.afisareMatrice();
+			}
 			break;
-		case 7: int option7;
-				int nr;
-				cout << "\nIntrodu numarul dorit pentru a inmulti matricea: ";
-				cin >> nr;
-				do
+		case 7:
+			int option7;
+			int nr;
+			cout << "\nIntrodu numarul dorit pentru a inmulti matricea: ";
+			cin >> nr;
+			do
+			{
+				cout << "\nAlege care matrice o inmultesti:"
+					<< "\n1. Patrata"
+					<< "\n2. Dreptunghiulara"
+					<< "\nOptiunea aleasa: ";
+				cin >> option7;
+				switch (option7)
 				{
-					cout << "\nAlege care matrice o inmultesti:"
-						<< "\n1. Patrata"
-						<< "\n2. Dreptunghiulara"
-						<< "\nOptiunea aleasa: ";
-					cin >> option7;
-					switch (option7)
-					{
-					case 1:
-						cout << "\nMatricea patrata inainte:\n";
-						p.afisareMatrice();
-						p.inmultireCuNumar(nr);
-						cout << "\nMatricea dupa:\n";
-						p.afisareMatrice();
-						break;
-					case 2:
-						cout << "\nMatricea dreptunghiulara inainte:\n";
-						d.afisareMatrice();
-						d.inmultireCuNumar(nr);
-						cout << "\nMatricea dupa:\n";
-						d.afisareMatrice();
-						break;
-					default: cout << "\nOptiune invalida! Incearca din nou.\n";
-						break;
-					}
-				} while (option7 != 1 && option7 != 2);
+				case 1:
+					cout << "\nMatricea patrata inainte:\n";
+					p.afisareMatrice();
+					p.inmultireCuNumar(nr);
+					cout << "\nMatricea dupa:\n";
+					p.afisareMatrice();
+					break;
+				case 2:
+					cout << "\nMatricea dreptunghiulara inainte:\n";
+					d.afisareMatrice();
+					d.inmultireCuNumar(nr);
+					cout << "\nMatricea dupa:\n";
+					d.afisareMatrice();
+					break;
+				default: cout << "\nOptiune invalida! Incearca din nou.\n";
+					break;
+				}
+			} while (option7 != 1 && option7 != 2);
 			break;
 		case 8: cout << "\nIesire din program...\n";
 			break;
@@ -342,6 +422,5 @@ int main()
 			break;
 		}
 	} while (optiune != 8);
-
 	return 0;
 }
